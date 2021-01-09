bf2558c48ac5bb8cc283b8020e28e910cd2d160f
Lucía Méndez López <UO250970@uniovi.es>
2021-01-09 18:54:53 +0100

    Chenged License

--- Travelogue_2/App.xaml.cs
+++ Travelogue_2/App.xaml.cs
@@ -14,7 +14,7 @@ namespace Travelogue_2
 
         public App()
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzI4NTQ5QDMxMzgyZTMzMmUzMGlTMHlEeHdwSGkyU3ZKb2dCcjArT0dtUXlUcmJkYzJnR2VPQzBsZm1kL0U9");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzgwNTEzQDMxMzgyZTM0MmUzMGd5ZzdMS2RINmM2NkNVQ0s0Y0szNElMS1Q0cmxoWW1nUmJIUlBiNDNBcE09");
             InitializeComponent();
 
             AppResources.Culture = new CultureInfo(string.Empty);
