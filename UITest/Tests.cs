d851964d4c194442fee4625b3a59722bfeb54dae
Soondra <UO250970@uniovi.es>
2021-05-06 08:37:15 +0200

    Starting navigation tests.

---
+++ UITest/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace UITest
+{
+    [TestFixture(Platform.Android)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        /** Navegación básica por las distintas lengüetas del menú */
+        [Test]
+        public void MenuNavigationTest()
+        {
+            app.Repl();
+        }
+    }
+}
