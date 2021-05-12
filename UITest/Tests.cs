9453ca7f6b63e9f684e05e35e620c112e081b85b
Lucía Méndez <32478830+UO250970@users.noreply.github.com>
2021-05-12 09:31:18 +0200

    Incio de ui tests completado. Continuar cuando tenga todos los Ids...

--- UITest/Tests.cs
+++ UITest/Tests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 using Xamarin.UITest;
 
 namespace UITest
@@ -17,14 +18,26 @@ namespace UITest
         [SetUp]
         public void BeforeEachTest()
         {
-            app = AppInitializer.StartApp(platform);
+            //app = ConfigureApp.Android.StartApp();
+
+            app = ConfigureApp.Android
+                 .ApkFile(@"C:\Users\lmendezl\AppData\Local\Xamarin\Mono for Android\Archives\2021-05-12\Travelogue_2.Android 5-12-21 9.09 AM.apkarchive\com.companyname.travelogue_2.apk")
+                 .DeviceSerial("6PQ0217821002256")
+                 .PreferIdeSettings()
+                 .EnableLocalScreenshots()
+                 .StartApp();
         }
 
         /** Navegación básica por las distintas lengüetas del menú */
         [Test]
         public void MenuNavigationTest()
         {
-            app.Repl();
+            app.Back();
+            //app.Repl();
+            app.WaitForElement("CreatedJourneysId", timeout: TimeSpan.FromSeconds(100));
+         
+            Assert.IsNotEmpty( app.Query("CreatedJourneysId") );
+            //app.Tap(x => x.Marked("MediaViewButton"));
         }
     }
 }
